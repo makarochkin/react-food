8abc2547b97653503b68be67eb171a7db99c5628
makarochkin <makarochkin.a.v@gmail.com>
2023-06-11 19:05:14 +0600

    project complete

--- src/api.js
+++ src/api.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { API_URL } from "./config";
 
 const getMealById = async (mealId) => {
--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+import React from "react";
 const API_KEY = "1";
 
 const API_URL = `https://www.themealdb.com/api/json/v1/${API_KEY}/`;
